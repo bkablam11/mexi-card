4d9668149d4c0a182117154cd3d6047541fff3b9
Mexiale Grace <gracemexiale@yahoo.fr>
2021-08-03 09:33:30 +0000

    Deployement

---
+++ app.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Author : Mexiale
+"""
+
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    if output == 1:
+        return render_template('Answers1.html', prediction_text = 'Le client va se désabonner')
+    else:
+        return render_template('Answers0.html', prediction_text='Le client ne va pas se désabonner')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force = True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
